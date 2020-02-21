66fda98236b478dd60c5ad30a12b8c6e577afa01
Benjamin Kampmann <ben@gnunicorn.org>
2020-02-21 19:22:06 +0100

    ignore 'unleash' as first argument

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,14 @@ mod util;
 use cli::Opt;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    cli::run(Opt::from_args())
+    let mut argv = Vec::new();
+    let mut args = std::env::args();
+    argv.extend(args.next());
+    if let Some(h) = args.next() {
+        if h != "unleash" {
+            argv.push(h)
+        }
+    }
+    argv.extend(args);
+    cli::run(Opt::from_iter(argv))
 }
