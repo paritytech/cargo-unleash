2860ec58a547a56e953fa68646e4bcb5af93f7ad
Benjamin Kampmann <ben@gnunicorn.org>
2020-02-27 18:12:49 +0100

    delay for publishing many packages

--- src/commands/release.rs
+++ src/commands/release.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::{thread, time::Duration};
 use cargo::{
     core::{
         package::Package, Workspace
@@ -22,8 +23,23 @@ pub fn release<'a>(
         index: None, jobs: None, target: None, registry: None, features: Vec::new(),
     };
 
+    let delay = {
+        if packages.len() > 29 {
+            // more than 30, delay so we do not publish more than 30 in 10min.
+            21
+        } else {
+            // below the limit we just burst them out.
+            0
+        }
+    };
+
     c.shell().status("Publishing", "Packages")?;
-    for pkg in packages {
+    for (idx, pkg) in packages.iter().enumerate() {
+        if idx > 0  && delay > 0 {
+            c.shell().status("Waiting", "published 30 crates – API limites require us to wait in between.")?;
+            thread::sleep(Duration::from_secs(delay));
+        }
+        
         let pkg_ws = Workspace::ephemeral(pkg.clone(), c, Some(ws.target_dir()), true)?;
         c.shell().status("Publishing", &pkg)?;
         publish(&pkg_ws, &opts)?;
