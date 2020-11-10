1b5a5b3fb0896abc138fc4bc5722dfe0d496e7c1
Benjamin Kampmann <ben@gnunicorn.org>
2020-11-10 20:28:57 +0100

    Calculate changed package since a git reference
    Squashed commit of the following:
    
    commit 4ec98b35bb82d1bebc4e83a9e0dd75ffa8ec4c3a
    Author: Benjamin Kampmann <ben@gnunicorn.org>
    Date:   Tue Oct 27 17:45:46 2020 +0100
    
        resolve git, better erroring
    
    commit a17b9fcbe1cabca72cae15b29432fe43ad6e8182
    Author: Benjamin Kampmann <ben@gnunicorn.org>
    Date:   Tue Oct 27 13:08:31 2020 +0100
    
        calculating the graph
    
    commit 5c18fd112a1598ce76fe5a03afaac49f6cbb15ec
    Merge: 9009841 9cbfd17
    Author: Benjamin Kampmann <ben@gnunicorn.org>
    Date:   Tue Oct 27 12:08:46 2020 +0100
    
        Merge remote-tracking branch 'origin/master' into ben-auto-bump
    
    commit 90098410c355cf99a623f40a5fe6b311d230a7e3
    Merge: ce42cac a9bed52
    Author: Benjamin Kampmann <ben@gnunicorn.org>
    Date:   Tue Oct 27 09:18:56 2020 +0100
    
        Merge branch 'ben-update-to-latest-cargo' into ben-auto-bump
    
    commit ce42cac5e7b7f1786ed5e42abb8a53ec9c929791
    Merge: 23b187e f8eeefc
    Author: Benjamin Kampmann <ben@gnunicorn.org>
    Date:   Tue Oct 27 09:14:14 2020 +0100
    
        Merge remote-tracking branch 'origin/master' into ben-auto-bump
    
    commit 23b187e978030ea4477e7212b0ca14bec94180cc
    Author: Benjamin Kampmann <ben@gnunicorn.org>
    Date:   Mon Sep 21 11:51:17 2020 +0200
    
        adding current state
    
    commit aff288f17bf32c3ebf5c6fce9467763b650c95d9
    Author: Benjamin Kampmann <ben@gnunicorn.org>
    Date:   Tue Sep 8 18:29:50 2020 +0200
    
        update to latest dependencies

--- src/cli.rs
+++ src/cli.rs
@@ -15,6 +15,7 @@ use structopt::StructOpt;
 use toml_edit::Value;
 
 use crate::commands;
+use crate::util;
 
 fn parse_identifiers(src: &str) -> Identifier {
     Identifier::AlphaNumeric(src.to_owned())
@@ -60,6 +61,14 @@ pub struct PackageSelectOptions {
     /// regardless, set this flag.
     #[structopt(long)]
     ignore_publish: bool,
+    /// Automatically detect the packages, which changed compared to the given git commit.
+    ///
+    /// Compares the current git `head` to the reference given, identifies which files changed
+    /// and attempts to identify the packages and its dependents through that mechanism. You
+    /// can use any `tag`, `branch` or `commit`, but you must be sure it is available
+    /// (and up to date) locally.
+    #[structopt(short, long)]
+    pub changed_since: String,
 }
 
 #[derive(StructOpt, Debug)]
@@ -341,19 +350,28 @@ pub struct Opt {
     pub cmd: Command,
 }
 
-fn make_pkg_predicate(args: PackageSelectOptions) -> Result<Box<dyn Fn(&Package) -> bool>, String> {
+fn make_pkg_predicate(ws: &Workspace<'_>, args: PackageSelectOptions) -> Result<Box<dyn Fn(&Package) -> bool>, String> {
     let PackageSelectOptions {
         packages,
         skip,
         ignore_pre_version,
         ignore_publish,
+        changed_since,
     } = args;
 
-    if !packages.is_empty() && (!skip.is_empty() || !ignore_pre_version.is_empty()) {
-        return Err(
-            "-p/--packages is mutually exlusive to using -s/--skip and -i/--ignore-version-pre"
-                .into(),
-        );
+    if !packages.is_empty() {
+        if !skip.is_empty() || !ignore_pre_version.is_empty() {
+            return Err(
+                "-p/--packages is mutually exlusive to using -s/--skip and -i/--ignore-version-pre"
+                    .into(),
+            );
+        }
+        if changed_since.len() != 0 {
+            return Err(
+                "-p/--packages is mutually exlusive to using -c/--changed-since"
+                    .into(),
+            );
+        }
     }
 
     let publish = move |p: &Package| {
@@ -363,6 +381,21 @@ fn make_pkg_predicate(args: PackageSelectOptions) -> Result<Box<dyn Fn(&Package)
 
         trace!("{:}.publish={}", p.name(), value);
         value
+    };
+
+    if changed_since.len() != 0 {
+        if !skip.is_empty() || !ignore_pre_version.is_empty() {
+            return Err(
+                "-c/--changed-since is mutually exlusive to using -s/--skip and -i/--ignore-version-pre"
+                    .into(),
+            );
+        }
+
+    }
+
+    let changed = util::changed_packages(ws, &changed_since)?;
+    if changed.len() == 0 {
+        return Err("No changes detected".into())
     };
 
     if !packages.is_empty() {
@@ -441,9 +474,9 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
             pkg_opts,
             check_only,
         } => {
-            let predicate = make_pkg_predicate(pkg_opts)?;
             let ws = Workspace::new(&root_manifest, &c)
                 .map_err(|e| format!("Reading workspace {:?} failed: {:}", root_manifest, e))?;
+            let predicate = make_pkg_predicate(&ws, pkg_opts)?;
             commands::clean_up_unused_dependencies(&ws, predicate, check_only)
         }
         Command::AddOwner {
@@ -451,9 +484,10 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
             token,
             pkg_opts,
         } => {
-            let predicate = make_pkg_predicate(pkg_opts)?;
             let ws = Workspace::new(&root_manifest, &c)
                 .map_err(|e| format!("Reading workspace {:?} failed: {:}", root_manifest, e))?;
+
+            let predicate = make_pkg_predicate(&ws, pkg_opts)?;
             for pkg in ws.members().filter(|p| predicate(p)) {
                 commands::add_owner(ws.config(), &pkg, owner.clone(), token.clone())?;
             }
@@ -468,7 +502,11 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
             if name == "name" {
                 return Err("To change the name please use the rename command!".into());
             }
-            let predicate = make_pkg_predicate(pkg_opts)?;
+
+            let ws = Workspace::new(&root_manifest, &c)
+                .map_err(|e| format!("Reading workspace {:?} failed: {:}", root_manifest, e))?;
+
+            let predicate = make_pkg_predicate(&ws, pkg_opts)?;
             let type_value = {
                 if let Ok(v) = bool::from_str(&value) {
                     Value::from(v)
@@ -479,8 +517,6 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                 }
             };
 
-            let ws = Workspace::new(&root_manifest, &c)
-                .map_err(|e| format!("Reading workspace {:?} failed: {:}", root_manifest, e))?;
             commands::set_field(
                 ws.members()
                     .filter(|p| predicate(p) && c.shell().status("Setting on", p.name()).is_ok()),
@@ -506,7 +542,7 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                     force_update,
                     version,
                 } => {
-                    let predicate = make_pkg_predicate(pkg_opts)?;
+                    let predicate = make_pkg_predicate(&ws, pkg_opts)?;
                     commands::set_version(
                         &ws,
                         |p| predicate(p),
@@ -518,7 +554,7 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                     pkg_opts,
                     force_update,
                 } => {
-                    let predicate = make_pkg_predicate(pkg_opts)?;
+                    let predicate = make_pkg_predicate(&ws, pkg_opts)?;
                     commands::set_version(
                         &ws,
                         |p| predicate(p),
@@ -547,7 +583,7 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                     pkg_opts,
                     force_update,
                 } => {
-                    let predicate = make_pkg_predicate(pkg_opts)?;
+                    let predicate = make_pkg_predicate(&ws, pkg_opts)?;
                     commands::set_version(
                         &ws,
                         |p| predicate(p),
@@ -564,7 +600,7 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                     pkg_opts,
                     force_update,
                 } => {
-                    let predicate = make_pkg_predicate(pkg_opts)?;
+                    let predicate = make_pkg_predicate(&ws, pkg_opts)?;
                     commands::set_version(
                         &ws,
                         |p| predicate(p),
@@ -581,7 +617,7 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                     pkg_opts,
                     force_update,
                 } => {
-                    let predicate = make_pkg_predicate(pkg_opts)?;
+                    let predicate = make_pkg_predicate(&ws, pkg_opts)?;
                     commands::set_version(
                         &ws,
                         |p| predicate(p),
@@ -599,7 +635,7 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                     pkg_opts,
                     force_update,
                 } => {
-                    let predicate = make_pkg_predicate(pkg_opts)?;
+                    let predicate = make_pkg_predicate(&ws, pkg_opts)?;
                     commands::set_version(
                         &ws,
                         |p| predicate(p),
@@ -616,7 +652,7 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                     pkg_opts,
                     force_update,
                 } => {
-                    let predicate = make_pkg_predicate(pkg_opts)?;
+                    let predicate = make_pkg_predicate(&ws, pkg_opts)?;
                     commands::set_version(
                         &ws,
                         |p| predicate(p),
@@ -632,7 +668,7 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                     pkg_opts,
                     force_update,
                 } => {
-                    let predicate = make_pkg_predicate(pkg_opts)?;
+                    let predicate = make_pkg_predicate(&ws, pkg_opts)?;
                     commands::set_version(
                         &ws,
                         |p| predicate(p),
@@ -647,16 +683,20 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                 }
             }
         }
-        Command::DeDevDeps { pkg_opts } => maybe_patch(false, &make_pkg_predicate(pkg_opts)?),
+        Command::DeDevDeps { pkg_opts } => {
+            let ws = Workspace::new(&root_manifest, &c)
+                .map_err(|e| format!("Reading workspace {:?} failed: {:}", root_manifest, e))?;
+            maybe_patch(false, &make_pkg_predicate(&ws, pkg_opts)?)
+        },
         Command::ToRelease {
             include_dev,
             pkg_opts,
         } => {
-            let predicate = make_pkg_predicate(pkg_opts)?;
-            maybe_patch(include_dev, &predicate)?;
-
             let ws = Workspace::new(&root_manifest, &c)
                 .map_err(|e| format!("Reading workspace {:?} failed: {:}", root_manifest, e))?;
+            let predicate = make_pkg_predicate(&ws, pkg_opts)?;
+            maybe_patch(include_dev, &predicate)?;
+
             let packages = commands::packages_to_release(&ws, predicate)?;
             println!(
                 "{:}",
@@ -679,11 +719,12 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
                 verify_readme_feature()?;
             }
 
-            let predicate = make_pkg_predicate(pkg_opts)?;
-            maybe_patch(include_dev, &predicate)?;
-
             let ws = Workspace::new(&root_manifest, &c)
                 .map_err(|e| format!("Reading workspace {:?} failed: {:}", root_manifest, e))?;
+
+            let predicate = make_pkg_predicate(&ws, pkg_opts)?;
+            maybe_patch(include_dev, &predicate)?;
+
             let packages = commands::packages_to_release(&ws, predicate)?;
 
             commands::check(&packages, &ws, build, check_readme)
@@ -693,11 +734,12 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
             pkg_opts,
             readme_mode,
         } => {
-            let predicate = make_pkg_predicate(pkg_opts)?;
-            maybe_patch(false, &predicate)?;
-
             let ws = Workspace::new(&root_manifest, &c)
                 .map_err(|e| format!("Reading workspace {:?} failed: {:}", root_manifest, e))?;
+
+            let predicate = make_pkg_predicate(&ws, pkg_opts)?;
+            maybe_patch(false, &predicate)?;
+
             let packages = commands::packages_to_release(&ws, predicate)?;
 
             commands::gen_all_readme(packages, &ws, readme_mode)
@@ -712,11 +754,10 @@ pub fn run(args: Opt) -> Result<(), Box<dyn Error>> {
             pkg_opts,
             check_readme,
         } => {
-            let predicate = make_pkg_predicate(pkg_opts)?;
-            maybe_patch(include_dev, &predicate)?;
-
             let ws = Workspace::new(&root_manifest, &c)
                 .map_err(|e| format!("Reading workspace {:?} failed: {:}", root_manifest, e))?;
+            let predicate = make_pkg_predicate(&ws, pkg_opts)?;
+            maybe_patch(include_dev, &predicate)?;
 
             let packages = commands::packages_to_release(&ws, predicate)?;
 
