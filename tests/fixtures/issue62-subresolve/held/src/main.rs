693adff8591278357cb3db386aee2536bccca8f9
Benjamin Kampmann <ben@gnunicorn.org>
2021-12-15 09:46:08 +0100

    adding test for #62

---
+++ tests/fixtures/issue62-subresolve/held/src/main.rs
@@ -0,0 +1,21 @@
+use parity_scale_codec_derive::codec::Encode;
+use sidekick::EnumType;
+
+fn main() {
+
+    let a = EnumType::A;
+    let b = EnumType::B(1, 2);
+    let c = EnumType::C { a: 1, b: 2 };
+
+    a.using_encoded(|ref slice| {
+        assert_eq!(slice, &b"\x0f");
+    });
+
+    b.using_encoded(|ref slice| {
+        assert_eq!(slice, &b"\x01\x01\0\0\0\x02\0\0\0\0\0\0\0");
+    });
+
+    c.using_encoded(|ref slice| {
+        assert_eq!(slice, &b"\x02\x01\0\0\0\x02\0\0\0\0\0\0\0");
+    });
+}
